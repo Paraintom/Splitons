5dd7d1dff78617bfc8e5aaa2ccd31da0b7f90e36
Thomas Barles <thomas.barles@gmail.com>
2015-03-11 18:50:52 +0000

    refactoring Balance object

--- SplitonsCoffeeScript/Balance.ts
+++ SplitonsCoffeeScript/Balance.ts
@@ -2,5 +2,5 @@
  * Created by Tom on 10/03/2015.
  */
 class Balance{
-    public constructor(public member:string, public balance:number){}
+    public constructor(public member:string, public amount:number){}
 }
--- SplitonsCoffeeScript/SplitonsApp.ts
+++ SplitonsCoffeeScript/SplitonsApp.ts
@@ -17,11 +17,11 @@ angular.module('splitonsApp', []).controller('FakeDataController', ['$scope', fu
         p.members.forEach(m=>result[m] = new Balance(m,0));
         //Computation
         p.transactions.forEach(t=>{
-            result[t.from].balance += t.amount;
+            result[t.from].amount += t.amount;
             var numberOfDebiter = t.to.length;
 
             t.to.forEach(debitor=> {
-                result[debitor].balance -= (t.amount / numberOfDebiter);
+                result[debitor].amount -= (t.amount / numberOfDebiter);
             });
         });
         return result;
@@ -36,11 +36,11 @@ function getFakeProject() {
     p.members.push("antoine");
     p.members.push("roger");
 
-    p.transactions.push(new Transaction("jean", ["emeline", "antoine"], "Beer", 21));
-    p.transactions.push(new Transaction("emeline", ["jean", "antoine", "roger"], "Cab", 32));
-    p.transactions.push(new Transaction("emeline", ["roger"], "chewing gum", 32));
-    p.transactions.push(new Transaction("emeline", ["antoine"], "dictionary", 12));
+    p.transactions.push(new Transaction("jean", ["emeline", "antoine"], "Beer", 20));
+    p.transactions.push(new Transaction("emeline", ["jean", "antoine", "roger"], "Cab", 30));
+    p.transactions.push(new Transaction("emeline", ["roger"], "chewing gum", 100));
+    /*p.transactions.push(new Transaction("emeline", ["antoine"], "dictionary", 12));
     p.transactions.push(new Transaction("roger", ["emeline"], "a brain", 12));
-    p.transactions.push(new Transaction("roger", ["antoine"], "Train", 12));
+    p.transactions.push(new Transaction("roger", ["antoine"], "Train", 12));*/
     return p;
 }
