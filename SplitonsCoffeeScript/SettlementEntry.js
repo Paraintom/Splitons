a1c87e767a378a00a0106c0d953e12d9d5258afb
George Goodchild <GeorgeGoodchild@GMail.com>
2015-11-14 11:39:02 +0000

    New home screen UI
    Use standard bootstrap
    Add files required to work in Visual Studio 2013

--- SplitonsCoffeeScript/SettlementEntry.js
+++ SplitonsCoffeeScript/SettlementEntry.js
@@ -1,6 +1,6 @@
 /**
- * Created by Tom on 11/03/2015.
- */
+* Created by Tom on 11/03/2015.
+*/
 var SettlementEntry = (function () {
     function SettlementEntry(from, to, amount, currency) {
         this.from = from;
