01bb2bee759b3d22150d75e599348d50cf654163
Thomas Barles <thomas.barles+github@gmail.com>
2016-07-14 12:50:16 +0100

    Cleanup - use factorised IsGuid

--- Factories/factories.ts
+++ Factories/factories.ts
@@ -34,7 +34,7 @@ projectsFactory.factory('projectsFactory', function () {
     }
 
     function isProjectId(key) {
-        return key.match("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}");
+        Guid.isGuid(key);
     }
 
 
